014cf30e1a60ff9f6dfa2af246e882e01680cf6a
Steve <urgent.programmer@gmail.com>
2017-03-29 10:30:08 -0400

    Removed boilerplate example component

--- src/components/Nav/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-require('./index.css');
-
-const Nav = <nav className="Nav"></nav>;
-
-module.exports = Nav;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ssr from './utilities/ssr/index';
-import Nav from './components/Nav/index';
 
 if (typeof document === 'object') {
   render(<h1>Hello world 7!</h1>, document.getElementById('root'));
 }
 
-ssr.transforms.push(ssr.insertBefore(Nav, '#root'));
+ssr.transforms.push(ssr.insertBefore(<h1>Hello 2</h1>, '#root'));
 
 export default ssr;
