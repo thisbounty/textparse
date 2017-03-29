6a797af8a7e505deb9c187d42403f29a8f33cac6
Steve <urgent.programmer@gmail.com>
2017-03-29 10:53:18 -0400

    Pagebreak -- Input include
    Added import to top level project.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ssr from './utilities/ssr/index';
+import Input from './components/Input/index';
 
 if (typeof document === 'object') {
-  render(<h1>Hello world 7!</h1>, document.getElementById('root'));
+  render(Input, document.getElementById('root'));
 }
 
 ssr.transforms.push(ssr.insertBefore(<h1>Hello 2</h1>, '#root'));
