dedd9d0909ec01213354bc71c01f0139720b15fc
Steve <urgent.programmer@gmail.com>
2017-03-29 20:17:13 -0400

    Pagebreak -- React Component
    Rewrote Input to extend react component. Removed rendering out of src/index
    and to src/components/Input for easier, self-contained components. Just have
    to add a require statement to src/index

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 require('./index.css');
 
-const Input = <div className="Input"></div>;
+class Input extends React.Component {
+  render() {
+    return (
+      <div id="Input" className="Input">
+        <label className="Input-textAreaLabel">Input</label>
+        <textarea id="Input-textArea" className="Input-textArea"></textarea>
+      </div>
+    );
+  }
+}
+
+if (typeof document === 'object') {
+  render(<Input />, document.getElementById('root'));
+}
 
 module.exports = Input;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
 import ssr from './utilities/ssr/index';
-import Input from './components/Input/index';
 
-if (typeof document === 'object') {
-  render(Input, document.getElementById('root'));
-}
-
-ssr.transforms.push(ssr.insertBefore(<h1>Hello 2</h1>, '#root'));
+require('./components/Input/index');
 
 export default ssr;
