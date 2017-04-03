8f4ca8fd02cef7715e92613290b046c0b84c2270
Steve <urgent.programmer@gmail.com>
2017-04-02 20:41:50 -0400

    Top Shortcode -- Top Shortcodes, Refactored to Methods
    Added top shortcodes to post. Ads for top of the article, first page ads are different than inner page ads
    
    Refactored existing code to individual methods. Instead of putting everything inside loop, clearer to have
    individual methods.

--- src/components/WpConvert/index.js
+++ src/components/WpConvert/index.js
@@ -12,6 +12,7 @@ export default class WpConvert extends React.Component {
       outpt: '',
     };
 
+    this.lineLoopData = {};
     this.handleChange = this.handleChange.bind(this);
   }
   render() {
@@ -33,32 +34,57 @@ export default class WpConvert extends React.Component {
   handleChange(event) {
     const text = event.target.value;
     this.setState({ input: text });
-    this.setState({ output: WpConvert.format(WpConvert.clean(text)) });
+    this.setState({ output: this.formatText(WpConvert.clean(text)) });
   }
 
   static clean(text) {
     return text.replace('\n\n\n', '\n\n').replace('\n\n', '\n');
   }
 
-  static format(text) {
-    let firstPageFlag = true;
-    let counter = 0;
-    let formattedText = '';
-    text.split('\n').forEach((line) => {
-      if (line.length === 0) {
-        return;
-      }
-      if (firstPageFlag && counter === 2) {
-        firstPageFlag = false;
-        formattedText += '<!--next page-->\n';
-        counter = 0;
-      } else if (!firstPageFlag && counter === 1) {
-        formattedText += '<!--next page-->\n';
-        counter = 0;
-      }
-      counter += 1;
-      formattedText += `${line}\n\n`;
-    });
-    return formattedText;
+  formatText(text) {
+    this.clearLineLoopData();
+    text.split('\n').forEach((line) => { this.formatLine(line); });
+    return this.lineLoopData.formattedText;
+  }
+
+  clearLineLoopData() {
+    this.lineLoopData = {
+      firstPageFlag: true,
+      counter: 0,
+      formattedText: '',
+    };
+  }
+
+  formatLine(line) {
+    if (!WpConvert.validateLine(line)) {
+      return;
+    }
+    this.topShortCodes();
+    this.addPageBreak(line);
+    this.lineLoopData.counter += 1;
+    this.lineLoopData.formattedText += `${line}\n\n`;
+  }
+
+  static validateLine(line) {
+    return (line.trim().length > 0);
+  }
+
+  topShortCodes() {
+    if (this.lineLoopData.firstPageFlag && this.lineLoopData.counter === 0) {
+      this.lineLoopData.formattedText += '[sc name="direct_default_top_ad" ]\n[sc name="default_top_ad" ]\n\n';
+    } else if (!this.lineLoopData.firstPageFlag && this.lineLoopData.counter === 0) {
+      this.lineLoopData.formattedText += '[sc name="default_top_ad" ]\n\n';
+    }
+  }
+
+  addPageBreak() {
+    if (this.lineLoopData.firstPageFlag && this.lineLoopData.counter === 2) {
+      this.lineLoopData.firstPageFlag = false;
+      this.lineLoopData.formattedText += '<!--next page-->\n';
+      this.lineLoopData.counter = 0;
+    } else if (!this.lineLoopData.firstPageFlag && this.lineLoopData.counter === 1) {
+      this.lineLoopData.formattedText += '<!--next page-->\n';
+      this.lineLoopData.counter = 0;
+    }
   }
 }
