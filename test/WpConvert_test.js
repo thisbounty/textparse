b88aa4c4cf2cc1d4c237eededdabbbd5d838e8b2
Steve <urgent.programmer@gmail.com>
2017-04-06 18:27:25 -0400

    Testable -- Indents, page shortcode tests
    Fixed indents for tests
    
    Created unit test for inserting page shortcodes

--- test/WpConvert_test.js
+++ test/WpConvert_test.js
@@ -12,27 +12,36 @@ const dummy = {
 const shortcodes = {
   pageBreak: '<!--next page-->',
   title: 'post_page_title',
-}
+  topPage: 'sc name="direct_default_top_ad"',
+  bottomPage: 'sc name="direct_default_lower_ad',
+};
 
 let wp = TestUtils.renderIntoDocument(<WpConvert />);
 wp.setState({'input':`${dummy.title}${dummy.newLine}${dummy.paragraph}${dummy.newLine}${dummy.title}${dummy.newLine}${dummy.paragraph}`});
 
 describe('WpConvert.insertPageBreaks', () => {
   it('should insert one page break between two pages', () => {
-  const pageBreaks = WpConvert.insertPageBreaks(wp.state.input);
-  assert.notEqual(pageBreaks, wp.state.input);
-  const regex = new RegExp(shortcodes.pageBreak,"g");
-  assert.equal((pageBreaks.match(regex)||[]).length, 1);
+    const pageBreaks = WpConvert.insertPageBreaks(wp.state.input);
+    assert.notEqual(pageBreaks, wp.state.input);
+    const regex = new RegExp(shortcodes.pageBreak,"g");
+    assert.equal((pageBreaks.match(regex)||[]).length, 1);
   });
 });
 
 describe('WpConvert.formatTitles', () => {
   it('should add title shortcodes around lines that have less than 70 characters', () => {
-  const titles = WpConvert.formatTitles(wp.state.input);
-  assert.notEqual(titles, wp.state.input);
-  // with simple regex, count will be: 2 shortcodes, for opening and closing * 2 titles in dummy input, so 4
-  // try this if more specific matching required: `\[${shortcodes.title}\].{0,70}\[\/${shortcode.title}\]`
-  const regex = new RegExp(shortcodes.title,"g");
-  assert.equal((titles.match(regex)||[]).length, 4);
+    const titles = WpConvert.formatTitles(wp.state.input);
+    assert.notEqual(titles, wp.state.input);
+    // with simple regex, count will be: 2 shortcodes, for opening and closing * 2 titles in dummy input, so 4
+    // try this if more specific matching required: `\[${shortcodes.title}\].{0,70}\[\/${shortcode.title}\]`
+    const regex = new RegExp(shortcodes.title,"g");
+    assert.equal((titles.match(regex)||[]).length, 4);
+  });
+});
+
+describe('WpConvert.pageShortcodes', () => {
+  it('should insert page shortcodes at the top and bottom of the page', () => {
+    const pageShortcodes = WpConvert.insertPageShortcodes(wp.state.input);
+    assert.equal(pageShortcodes, `${shortcodes.topPage}\n${wp.state.input}\n${shortcodes.bottomPage}`);
   });
 });
