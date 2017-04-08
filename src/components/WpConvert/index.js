826266df9302c93036266ffa6882ef935a481079
Steve <urgent.programmer@gmail.com>
2017-04-08 00:16:44 -0400

    Format Fixes -- Varying inner page paragraph count
    Detected titles for page break and shortcode insertion.
    
    Removed paragraph detection for lines of 70 characters or greater. Inner page paragraph count can change.
    Easiest way to adjust for that is to look for the titles of 70 character or less, and use those for placement.
    
    Regex gets complicated so that it can ignore shortcodes, detect the first and last pages when adding pagebreaks,
    and ignore new lines when inserting new text

--- src/components/WpConvert/index.js
+++ src/components/WpConvert/index.js
@@ -38,37 +38,46 @@ export default class WpConvert extends React.Component {
 
   static parse(text) {
     const _ = WpConvert;
-    return _.insertArticleShortcodes(_.insertLowerPageShortcodes((_.insertPageBreaks(_.formatTitles(_.insertTopPageShortcodes(text))))));
+    return _.insertArticleShortcodes(_.formatTitles(_.insertTopPageShortcodes(_.insertPageBreaks(_.insertLowerPageShortcodes(text)))));
   }
 
   static insertTopPageShortcodes(text) {
     // match titles with 3 to 70 characters
     // 3 characters to avoid matching carriage returns
     // 70 as this will be run before format titles
-    return text.replace(/^.{3,70}$/gm, match => `[sc name="default_top_ad"]\n\n${match}\n`);
+    return text.replace(/^[^\[<!--].{3,70}$/gm, match => `[sc name="default_top_ad"]\n${match}`);
+  }
+
+  static insertLowerPageShortcodes(text) {
+    // match titles, allows for pages with varying paragraph counts
+    // match a leading carriage return to prevent adding a lower title at the start of the article
+    // no leading carrot allows for single or double space before title
+    text = text.replace(/\n^[^\[].{3,70}$/gm, match => `\n[sc name="default_lower_ad"]${match}`);
+    return `${text}\n[sc name="default_lower_ad"]`;
+  }
+
+  static insertPageBreaks(text) {
+    // find titles and add pagebreak before. Helps with pages of varying paragraph count
+    // match a leading \n to prevent adding a pagebreak at the top of the article
+    // no leading carrot allows for single or double space before title
+    // complicated regex, see https://regex101.com/r/M2pXOL/1
+    // goal of the regex is to look for titles and add a pagebreak on the line before it
+    // first \n is to prevent a page break at the top of the first page
+    // exclude in brackets [] prevents other shortcodes => \[ from being treated as a title
+    // exclude \n prevents extra shortcodes from being entered as spaces
+    return text.replace(/\n^[^\[].{3,70}$/gm, match => `\n<!--next page-->\n${match}`);
   }
 
   static formatTitles(text) {
     // match lines with 3 to 70 characters, for titles, multiline to get them all
     // 3 characters to avoid matching carriage returns
     // skip fields that have shortcodes already with [^\]]
-    // dot selector brings in carriage return, need to exclude with [^\n], added to closing bracket
-    return text.replace(/^[^\[\n].{3,70}$/gm, (match) => `[post_page_title]${match.replace('','')}[/post_page_title]` );
-  }
-
-  static insertPageBreaks(text) {
-    // match text with characters of 104 or more, and ending with a \n
-    // 70 character min, with two title tags of 17 characters each
-    // \n in regex so last page has no pagebreak
-    return text.replace(/.{104,}\n/g, match => `${match}\n<!--next page-->\n`);
+    // dot selector brings in carriage return to match, need to exclude with [^\n], added to closing bracket
+    return text.replace(/^[^\[\n\<!].{3,70}$/gm, (match) => `[post_page_title]${match.replace('','')}[/post_page_title]` );
   }
 
   static insertArticleShortcodes(text) {
+    // need shortcodes at the top and bottom of the entire article, simple concat
     return `[sc name="direct_default_top_ad"]\n${text}\n[sc name="direct_default_lower_ad"]`;
-  }
-
-  static insertLowerPageShortcodes(text) {
-    // match text with more than 104 characters
-    return text.replace(/^.{104,}$/gm, match => `${match}\n\n[sc name="default_lower_ad"]`);
   }
 }
