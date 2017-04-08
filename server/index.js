d29c399965e42adb01f2e34cbf4c9f9bdd1d8172
Steve <urgent.programmer@gmail.com>
2017-04-08 11:16:42 -0400

    SSR -- Server from web root
    /ssr route existed because static html was overriding it with index.html
    from build/
    
    Unlike nginx, top most route in express.js has precedence. Moving the
    app.use statement below the routes stops index.html from overriding
    the /ssr route, and now /ssr can be served from /

--- server/index.js
+++ server/index.js
@@ -19,13 +19,13 @@ fs.readFile('./build/index.html', 'utf8', function (err, html) {
     // start serving express
     var app = express();
 
-    app.get('/ssr', function (req, res) {
+    app.get('/', function (req, res) {
       tidy.tidy(transformed.document.documentElement.innerHTML, {doctype: 'html5',hideComments: false, indent: true},
       function(err, html) {
         res.send(html);
       });
     });
-	
+
 	app.post('/images', function (req, res) {
 	  var scraper = new Scraper.Google();
 	  scraper.list({
@@ -38,9 +38,9 @@ fs.readFile('./build/index.html', 'utf8', function (err, html) {
 		res.send(JSON.stringify(err));
 	  });
 	});
-	
+
 	app.use(express.static(process.cwd() + '/build'));
-	
+
     app.listen(3000, function () {
       console.log('SSR listening on port 3000!\nMake sure you are not viewing a static page')
     })
